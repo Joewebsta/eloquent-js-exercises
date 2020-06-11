9add011a0fcbac553fe357287195c6486efbfdd8
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-10 20:44:57 -0400

    Complete chessboard exercise

--- chap02-program-structure/chessboard.js
+++ chap02-program-structure/chessboard.js
@@ -1,1 +1,21 @@
-console.log('chessboard - it works!');
+let output = '';
+let pound = false;
+let size = 8;
+
+for (i = 1; i <= (size ** 2); i++) {
+
+  if (pound) {
+    output += '#';
+    pound = !pound;
+  } else {
+    output += ' ';
+    pound = !pound;
+  }
+
+  if (i % size === 0) {
+    output += '\n';
+    pound = !pound;
+  }
+}
+
+console.log(output);
