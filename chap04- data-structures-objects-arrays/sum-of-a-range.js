7c5f791f083cd062551c5b3ded021b36b37f0b86
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-12 16:13:05 -0400

    Complete Sum of a range exercise

---
+++ chap04- data-structures-objects-arrays/sum-of-a-range.js
@@ -0,0 +1,30 @@
+function range(start, end, step = 1) {
+  let range = [];
+
+  if ( step < 0){
+    for (let i = start; i >= end; i += step) {
+      range.push(i);
+    }
+  } else {
+    for (let i = start; i <= end; i += step) {
+      range.push(i);
+    }
+  }
+  return range;
+}
+
+function sum(numbers) {
+  let count = 0;
+
+  for (number of numbers) {
+    count += number;
+  }
+
+  return count;
+}
+
+console.log(range(1, 10, 2));
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
+console.log(sum(range(1, 100, 5)));
